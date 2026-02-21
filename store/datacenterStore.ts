0aa3a374253949e0e3b50267c7a81104385a0b35
Tyler Irving <tyler.i.developer@gmail.com>
2026-02-20 20:40:09 -0600

    Fix: Remove hexagon imports from datacenterStore

--- store/datacenterStore.ts
+++ store/datacenterStore.ts
@@ -1,49 +1,23 @@
 // Zustand store for datacenter data
 import { create } from 'zustand';
-import { HexagonData } from '@/types/hexagon';
 import { Datacenter } from '@/types/datacenter';
 
 interface DatacenterState {
   // Data
-  hexagons: Map<string, HexagonData>;
   datacenters: Datacenter[];
   lastFetched: number | null;
   
   // Actions
-  setHexagons: (hexagons: HexagonData[]) => void;
   setDatacenters: (datacenters: Datacenter[]) => void;
-  updateHexagon: (h3Index: string, updates: Partial<HexagonData>) => void;
-  getHexagon: (h3Index: string) => HexagonData | undefined;
   clearCache: () => void;
 }
 
-export const useDatacenterStore = create<DatacenterState>((set, get) => ({
-  hexagons: new Map(),
+export const useDatacenterStore = create<DatacenterState>((set) => ({
   datacenters: [],
   lastFetched: null,
   
-  setHexagons: (hexagons) =>
-    set({
-      hexagons: new Map(hexagons.map(h => [h.h3Index, h])),
-      lastFetched: Date.now(),
-    }),
-  
   setDatacenters: (datacenters) =>
-    set({ datacenters }),
+    set({ datacenters, lastFetched: Date.now() }),
   
-  updateHexagon: (h3Index, updates) =>
-    set((state) => {
-      const newMap = new Map(state.hexagons);
-      const existing = newMap.get(h3Index);
-      if (existing) {
-        newMap.set(h3Index, { ...existing, ...updates });
-      }
-      return { hexagons: newMap };
-    }),
-  
-  getHexagon: (h3Index) => {
-    return get().hexagons.get(h3Index);
-  },
-  
-  clearCache: () => set({ hexagons: new Map(), lastFetched: null }),
+  clearCache: () => set({ datacenters: [], lastFetched: null }),
 }));
