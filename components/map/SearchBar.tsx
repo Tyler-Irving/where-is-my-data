e57d9d71b68105b99e8603344bb3dfa388cfbc14
Tyler Irving <tyler.i.developer@gmail.com>
2026-02-20 20:27:51 -0600

    Fix TypeScript error: Remove padding from ViewportState

--- components/map/SearchBar.tsx
+++ components/map/SearchBar.tsx
@@ -69,7 +69,6 @@ export const SearchBar = React.memo(function SearchBar({ onSearchResults, onResu
       zoom: 8,
       bearing: 0,
       pitch: 0,
-      padding: { top: 0, bottom: 0, left: 0, right: 0 },
     });
     
     // Select datacenter
